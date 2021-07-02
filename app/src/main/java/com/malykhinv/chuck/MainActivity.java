761a0968066f7a3e09307e19c7ae432e5c9c8b2d
malykhinv <malykhinv@yandex.ru>
2021-07-02 18:44:56 +0300

    Create app project

---
+++ app/src/main/java/com/malykhinv/chuck/MainActivity.java
@@ -0,0 +1,14 @@
+package com.malykhinv.chuck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
