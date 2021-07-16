8494a7f5f3ff405fad1cdfbdccd9af4e10266de9
malykhinv <malykhinv@yandex.ru>
2021-07-16 20:13:01 +0300

    Remove Jsoup from the project
    Because of the url change there is a better approach to get jokes.

--- app/src/main/java/com/malykhinv/chuck/jokes/mvp/model/JokesModel.java
+++ app/src/main/java/com/malykhinv/chuck/jokes/mvp/model/JokesModel.java
@@ -5,11 +5,6 @@ import android.content.SharedPreferences;
 
 import com.malykhinv.chuck.di.App;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
 import java.util.ArrayList;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
@@ -22,11 +17,11 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class JokesModel {
 
-    private static final String URL_TO_PARSE = "https://anekdot-book.com/podborka/anekdoty-pro-chak-norrisa";
+    private static final String URL_TO_PARSE = "http://www.icndb.com/the-jokes-2/";
     private static final String USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0";
     private static final String URL_REFERRER = "https://yandex.ru/";
-    private static final String HTML_TAG_TO_SELECT = "div";
-    private static final String HTML_ATTRIBUTE_VALUE = "ads-color-box";
+    private static final String HTML_TAG_TO_SELECT = "tr";
+    private static final String HTML_ATTRIBUTE_VALUE = "jokes-odd";
     private static final String APP_PREFERENCES = "DATA";
     private static final String DEFAULT_SP_VALUE = "empty";
     private final Context context = App.getAppComponent().getContext();
@@ -83,23 +78,25 @@ public class JokesModel {
     }
 
     private ArrayList<String> loadListOfJokesFromWebsite(int countOfJokes) {
-        try {
-            final Document doc = Jsoup.connect(URL_TO_PARSE)
-                    .userAgent(USER_AGENT)
-                    .referrer(URL_REFERRER)
-                    .get();
-
-            final Elements docElements = doc.select(HTML_TAG_TO_SELECT + "." + HTML_ATTRIBUTE_VALUE);
-
-            int limitOfJokes = Math.min(countOfJokes, docElements.size());
-
-            for (int i = 0; i < limitOfJokes; i++) {
-                String joke = docElements.get(i).text();
-                listOfJokes.add(joke);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            final Document doc = Jsoup.connect(URL_TO_PARSE)
+//                    .userAgent(USER_AGENT)
+//                    .referrer(URL_REFERRER)
+//                    .timeout(3000)
+//                    .get();
+//
+//            final Elements docElements = doc.select(HTML_TAG_TO_SELECT + "." + HTML_ATTRIBUTE_VALUE);
+//
+//            int limitOfJokes = Math.min(countOfJokes, docElements.size());
+//
+//            for (int i = 0; i < limitOfJokes; i++) {
+//                String joke = docElements.get(i).text();
+//                listOfJokes.add(joke);
+//
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
         return listOfJokes;
     }
