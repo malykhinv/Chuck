72a5ae52be9d96f189a5e79c45de317d09c913c9
malykhinv <malykhinv@yandex.ru>
2021-07-17 15:40:50 +0300

    Add webview implementation

---
+++ app/src/main/java/com/malykhinv/chuck/webview/CustomWebViewClient.java
@@ -0,0 +1,14 @@
+package com.malykhinv.chuck.webview;
+
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class CustomWebViewClient extends WebViewClient {
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        view.loadUrl(request.getUrl().toString());
+        return true;
+    }
+}
--- app/src/main/java/com/malykhinv/chuck/webview/WebviewFragment.java
+++ app/src/main/java/com/malykhinv/chuck/webview/WebviewFragment.java
@@ -1,5 +1,7 @@
 package com.malykhinv.chuck.webview;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,11 +10,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.malykhinv.chuck.R;
+import com.malykhinv.chuck.databinding.FragmentWebviewBinding;
+
+import org.jetbrains.annotations.NotNull;
 
 public class WebviewFragment extends Fragment {
 
-    public WebviewFragment() {}
+    private static final String DEFAULT_URL = "http://www.icndb.com/api/";
+    private FragmentWebviewBinding b;
+    private View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,8 +26,27 @@ public class WebviewFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_webview, container, false);
+
+        b = FragmentWebviewBinding.inflate(inflater, container, false);
+
+        if (view == null) {
+            view = b.getRoot();
+        } else if (view.getParent() != null) {
+            ((ViewGroup) view.getParent()).removeView(view);
+        }
+
+        return view;
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        b.webView.getSettings().setJavaScriptEnabled(true);
+        b.webView.setWebViewClient(new CustomWebViewClient());
+        b.webView.loadUrl(DEFAULT_URL);
     }
 }
